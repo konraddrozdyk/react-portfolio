c8fef9e0b09ff21fb5616e411e3b7cab92cfdbb6
konraddrozdyk <konrad.drozdyk@gmail.com>
2024-12-15 14:36:45 +0100

    Minor adjustments to component structure

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -16,22 +16,27 @@ interface ProjectGridProps {
 
 const ProjectGrid: React.FC<ProjectGridProps> = ({ projects }) => {
   return (
-    <div
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 bg-transparent"
-      id="projects"
-    >
-      {projects.map((project, index) => (
-        <ProjectCard
-          key={index}
-          title={project.title}
-          description={project.description}
-          link={project.link}
-          year={project.year}
-          imageUrl={project.imageUrl}
-          detailedDescription={project.detailedDescription}
-        />
-      ))}
-    </div>
+    <>
+      <h2 className="flex flex-auto justify-center text-4xl pb-5">
+        Highlights
+      </h2>
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 bg-transparent"
+        id="projects"
+      >
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+            year={project.year}
+            imageUrl={project.imageUrl}
+            detailedDescription={project.detailedDescription}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
