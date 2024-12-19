ff8270ecd2aa8ce4599ad4e90cd161e3566faddf
konraddrozdyk <konrad.drozdyk@gmail.com>
2024-12-19 14:27:41 +0100

    Font changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -37,18 +37,17 @@ const AboutMe: React.FC = () => {
           className="rounded-full w-28 h-28 shadow-lg border-4 mb-6 bg-opacity-50"
         />
         <div className="text-center">
-          <h2 className="text-4xl font-bold mb-4">About Me</h2>
+          <h2 className="text-4xl mb-4">About Me</h2>
           <p className="text-lg leading-relaxed mb-6">
-            Hi, I'm{" "}
-            <span className="font-semibold text-blue-600">[Your Name]</span>, a
+            Hi, I'm <span className="text-blue-600">[Your Name]</span>, a
             passionate{" "}
-            <span className="font-semibold">Full-Stack Developer</span> with a
+            <span className="font-garamond">Full-Stack Developer</span> with a
             knack for building robust applications and crafting efficient test
             automation strategies. With{" "}
-            <span className="font-semibold">X years of experience</span>, I
-            specialize in <span className="font-semibold">.NET</span>,{" "}
-            <span className="font-semibold">Test & Automation</span>, and{" "}
-            <span className="font-semibold">.NET</span>. My mission is to
+            <span className="font-garamond">X years of experience</span>, I
+            specialize in <span className="font-garamond">.NET</span>,{" "}
+            <span className="font-garamond">Test & Automation</span>, and{" "}
+            <span className="font-garamond">.NET</span>. My mission is to
             deliver high-quality solutions that bridge the gap between
             innovation and functionality.
           </p>
@@ -56,15 +55,15 @@ const AboutMe: React.FC = () => {
 
         <div className="flex flex-wrap justify-center gap-4 mt-8">
           <div className="bg-santafe text-gray-800 p-4 rounded-lg shadow-lg w-48 text-center">
-            <h3 className="text-2xl font-semibold text-blue-600">5+</h3>
+            <h3 className="text-2xl text-blue-600">5+</h3>
             <p>Years of Experience</p>
           </div>
           <div className="bg-quincy text-gray-800 p-4 rounded-lg shadow-lg w-48 text-center">
-            <h3 className="text-2xl font-semibold text-blue-600">20+</h3>
+            <h3 className="text-2xl">20+</h3>
             <p>Completed Projects</p>
           </div>
           <div className="bg-costa text-gray-800 p-4 rounded-lg shadow-lg w-48 text-center">
-            <h3 className="text-2xl font-semibold text-blue-600">Key Skills</h3>
+            <h3 className="text-2xl">Key Skills</h3>
             <p>Test & Automation, .NET</p>
           </div>
         </div>
@@ -78,7 +77,7 @@ const AboutMe: React.FC = () => {
             <button
               key={tab.id}
               onClick={() => setActiveTab(tab.id)}
-              className={`px-4 py-2 rounded-lg font-semibold transition-all ${
+              className={`px-4 py-2 rounded-lg transition-all ${
                 activeTab === tab.id
                   ? "bg-costa text-white"
                   : "bg-quincy hover:bg-gray-300"
