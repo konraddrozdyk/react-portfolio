cf89d0202d2449c0024d06ac0b102a3e98691d16
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 15:16:45 +0100

    Added cv

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,7 +5,11 @@ const Resume: React.FC = () => (
   >
     <div className="container mx-auto text-center">
       <h2 className="text-3xl mb-6">Download My Resume</h2>
-      <a href="/path-to-resume.pdf" className=" py-2 px-6 rounded">
+      <a
+        href="./assets/KonradDrozdyk.pdf"
+        download="KonradDrozdyk_Resume.pdf"
+        className="py-2 px-6 rounded bg-costa text-white hover:bg-seablue"
+      >
         Download Resume
       </a>
     </div>
